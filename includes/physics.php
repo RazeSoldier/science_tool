8d31d4b769f19f9061e30a21e825bafe117ddbf1
gakuencity <liguangjie4399@hotmail.com>
2017-07-06 15:41:26 +0800

    对象化"相对论质量"计算

--- includes/physics.php
+++ includes/physics.php
@@ -1,8 +1,20 @@
-﻿<?php
-class  PhysicsObject{ //史瓦西半径计算
-	public function schwarzschild($m){
+<?php
+class  PhysicsObject{ //物理计算
+    /*声明常量*/
+    const SPEED_OF_LIGHT = 299792458; //声明光速(常量)
+	
+	/*声明错误信息(变量)*/
+	public $error1 = "<title>错误 - 工具箱/学园都市</title>您所输入的速度值<b>大于等于</b>光速(299792458 m·s<sup>-1</sup>)</br>
+	请确保您输入的速度值小于光速</br>
+	<a href=\"JavaScript:history.go(-1)\">返回</a>";
+    public $error2 = "<title>错误 - 工具箱/学园都市</title>您输入的数值包含了<b>非正数</b>数字</br>
+	请确保您输入的数字<b>大于0</b></br>
+	<a href=\"JavaScript:history.go(-1)\">返回</a>
+	";
+	
+	public function schwarzschild($m){ //史瓦西半径计算
 		/*声明初始变量*/
-		$c = 299792458; //声明光速(变量)
+		$c = self::SPEED_OF_LIGHT; //声明光速(变量)
 		$gc = 6.67408E-11; //声明万有引力常数(变量)
 		
 		$c2 = $c * $c; //计算光速平方
@@ -10,15 +22,13 @@ class  PhysicsObject{ //史瓦西半径计算
 		$result = $up / $c2; //计算$up/c^2
 		
 		/*输出变量*/
-		$output = "
-		<title>史瓦西半径计算结果/学园都市</title>
-		<b>条件量</b></br>
+		$output = "<title>史瓦西半径计算结果/学园都市</title><b>条件量</b></br>
 		天体质量:$m kg</br><hr/>
 		<b>计算结果</b>:$result m
 		";
 
 		if($m <= 0){ //判断1个输入数值是否小于等于0
-			return require_once "../includes/gravity/error_2.php"; //如果ture，则返回错误信息"error_2.php"
+			return $this->error2; //如果ture，则返回错误信息"$error2"
 		}else{
 			return $output; //输出结果
 		}
@@ -26,7 +36,7 @@ class  PhysicsObject{ //史瓦西半径计算
 	
 	public function time_dilation($t,$v){
 		/*声明初始变量*/
-		$c = 299792458; //声明光速(变量)
+		$c = self::SPEED_OF_LIGHT; //声明光速(变量)
 		
 		$c2 = $c * $c; //计算光速平方
 		$v2 = $v * $v; //计算物体速度的平方
@@ -37,17 +47,42 @@ class  PhysicsObject{ //史瓦西半径计算
 		$lorentz_factor = 1 / $down; //计算洛伦兹因子
 		
 		/*输出变量*/
-		$output = "
-		<title>相对论质量计算结果/学园都市</title>
-		<b>条件量</b></br>
+		$output = "<title>时间膨胀计算结果/学园都市</title><b>条件量</b></br>
 		物体的原时:$t s</br>
 		物体的速度:$v m·s<sup>-1</sup></br><hr/>洛伦兹因子:$lorentz_factor</br><b>计算结果</b>:$result s
 		";
 		
 		if($v >= $c){ ////判断"物体速度"是否大于等于光速
-		    return require_once "../includes/gravity/error_1.php"; //如果ture，则返回错误信息"error_1.php"
+		    return $this->error1; //如果ture，则返回错误信息"$error2"
 			}else if(($t <= 0) or ($v <= 0)){ //判断2个输入值是否有小于等于0的数值
-			return require_once "../includes/gravity/error_2.php"; //如果ture，则返回错误信息"error_2.php"
+			return $this->error2; //如果ture，则返回错误信息"$error2"
+			}else{ 
+			return $output;
+			}
+	}
+	
+	public function relativistic_mass($m,$v){
+		/*声明初始变量*/
+		$c = self::SPEED_OF_LIGHT; //声明光速(变量)
+		
+		$c2 = $c * $c; //计算光速平方
+		$v2 = $v * $v; //计算物体速度的平方
+		$y = $v2 / $c2; //计算(v^2/c^2)
+		$down = sqrt(1 - $y); //计算√(1-(v^2/c^2))
+		$result = $m / $down;
+		
+		$lorentz_factor = 1 / $down; //计算洛伦兹因子
+		
+		/*输出变量*/
+		$output = "<title>相对论质量计算结果/学园都市</title><b>条件量</b></br>
+		物体的质量:$m kg</br>
+		物体的速度:$v m·s<sup>-1</sup></br><hr/>洛伦兹因子:$lorentz_factor</br><b>计算结果</b>:$result kg
+		";
+		
+		if($v >= $c){ ////判断"物体速度"是否大于等于光速
+		    return $this->error1; //如果ture，则返回错误信息"$error2"
+			}else if(($m <= 0) or ($v <= 0)){ //判断2个输入值是否有小于等于0的数值
+			return $this->error2; //如果ture，则返回错误信息"$error2"
 			}else{ 
 			return $output;
 			}
--- physics/resource/relativistic_mass.php
+++ physics/resource/relativistic_mass.php
@@ -1,27 +1,5 @@
 ﻿<?php
-/*声明变量*/
-$c = 299792458; //光速
-$m0 = $_POST["rmm0"]; //用户输入量"物体的质量"
-$v = $_POST["rmv"]; //用户输入量"物体的速度"
+require_once "..\includes\physics.php";
 
-$c2 = $c * $c; //计算光速的平方
-
-$v2 = $v * $v; //计算物体速度的平方
-
-$y = $v2 / $c2; //计算(v^2/c^2)
-$down = sqrt(1 - $y); //计算√(1-(v^2/c^2))
-$result = $m0 / $down;
-
-$lorentz_factor = 1 / $down; //计算洛伦兹因子
-
-if($v >= $c){ //判断"物体速度"是否大于等于光速
-	require_once "../includes/gravity/error_1.php"; //如果ture，则返回错误信息"error_1.php"
-}else if(($m0 <= 0) or ($v <= 0)){ //判断2个输入值是否有小于等于0的数值
-	require_once "../includes/gravity/error_2.php"; //如果ture，则返回错误信息"error_2.php"
-}else{ 
-echo "<title>相对论质量计算结果/学园都市</title>";
-echo "<b>条件量</b></br>";
-echo "物体的质量:$m0 kg</br>";
-echo "物体的速度:$v m·s<sup>-1</sup></br>";
-echo "<hr/>洛伦兹因子:$lorentz_factor</br><b>计算结果</b>:$result kg"; //输出结果
-}
+$str = new PhysicsObject();
+echo $str->relativistic_mass($_POST["rmm0"],$_POST["rmv"]);
