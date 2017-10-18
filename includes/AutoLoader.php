c70391af26b530dff258dacc52b1ef1f77edad3c
gakuencity <liguangjie4399@hotmail.com>
2017-10-18 15:29:44 +0800

    [#4]修复linux下，AutoLoader无法工作的问题

--- includes/AutoLoader.php
+++ includes/AutoLoader.php
@@ -21,16 +21,86 @@
  */
 
 class AutoLoader{
+	/**
+	 * @var string $phpExt 普通类文件的扩展名
+	 */
+	private $phpExt = '.php';
+
+	/**
+	 * @var string $classphpExt 类文件的扩展名
+	 */
+	private $classExt = '.class.php';
+
+	/**
+	 * @var string $subclassExt 子类文件的扩展名
+	 */
+	private $subclassExt = '.subclass.php';
+
+	/**
+	 * @var array $subDir includes文件夹下拥有类文件的文件夹名称
+	 */
+	private $subDir = array('encrypt', 'health', 'physics');
+
+	/**
+	 * @var int $subCount $subDir数组的键值个数
+	 */
+	private $subCount;
+
+	public function __construct(){
+		$this->subCount = count($this->subDir);
+	}
+
+	/**
+	 * 在includes文件夹里搜索是否有匹配的类文件
+	 *
+	 * @return boolean
+	 */
+    private function findRoot($classname){
+        if (file_exists(INCLUDES_PATH.$classname.$this->phpExt)){
+        	require_once INCLUDES_PATH.$classname.$this->phpExt;
+        	return true;
+        }else if(file_exists(INCLUDES_PATH.$classname.$this->classExt)){
+        	require_once INCLUDES_PATH.$classname.$this->classExt;
+        	return true;
+        }else if(file_exists(INCLUDES_PATH.$classname.$this->subclassExt)){
+        	require_once INCLUDES_PATH.$classname.$this->subclassExt;
+        	return true;
+        }else{
+        	return false;
+        }
+    }
+
     /**
-     * 设定spl_autoload_register的默认文件扩展名
+     * 在includes下面特地的子文件夹里搜索是否有匹配的类文件
      */
-    private function setSplExtension(){
-        spl_autoload_extensions('.class.php,.subclass.php,.php');
+    private function findSubDir($classname){
+    	for ($i = 0; $i < $this->subCount; $i++){
+    		$subDirname = $this->subDir[$i];
+	    	if (file_exists(INCLUDES_PATH.$subDirname.'/'.$classname.$this->phpExt)){
+	        	require_once INCLUDES_PATH.$subDirname.'/'.$classname.$this->phpExt;
+	        	$i = -1;
+	        }else if(file_exists(INCLUDES_PATH.$subDirname.'/'.$classname.$this->classExt)){
+	        	require_once INCLUDES_PATH.$subDirname.'/'.$classname.$this->classExt;
+	        	$i = -1;
+	        }else if(file_exists(INCLUDES_PATH.$subDirname.'/'.$classname.$this->subclassExt)){
+	        	require_once INCLUDES_PATH.$subDirname.'/'.$classname.$this->subclassExt;
+	        	$i = -1;
+	        } //end if
+	        if ($i == -1){
+	        	break;
+	        }
+    	} //end for
+    }
+
+    private function loadClass($classname){
+    	$findRootResult = $this->findRoot($classname);
+    	if (!$findRootResult){
+    		$this->findSubDir($classname);
+    	}
     }
     
     final public function autoLoading(){
-        $this->setSplExtension();
-        spl_autoload_register();
+        spl_autoload_register(array($this, 'loadClass'));
     }
 }
 
