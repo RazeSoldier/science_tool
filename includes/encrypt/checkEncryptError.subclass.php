3a0feffc0d97c869a744982d3d2eada2a56800b6
RazeSoldier <liguangjie4399@hotmail.com>
2017-10-28 14:24:42 +0800

    修复一个Warning级别的错误(直接访问index.php?title=encrypt&action=result的时候会报错)

--- includes/encrypt/checkEncryptError.subclass.php
+++ includes/encrypt/checkEncryptError.subclass.php
@@ -3,7 +3,7 @@
 class checkEncryptError extends checkError {
     public function checkRadioValue($value){ //检查用户是否选定单选项(error11)
         if ($value == NULL){ //如果用户为未选择单选，终止脚本
-            die (checkError()->return11());
+            die ($this->return11());
         }
     }
 }
