19769fd32684c8bb733a022f8cdb0104283b2d96
RazeSoldier <liguangjie4399@hotmail.com>
2017-11-25 21:35:00 +0800

    [PSR] 修复行结尾有空格的问题

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-/** 
+/**
  * 入口文件
- * 
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
