dca8189e8c5e51815056e17dbcd4e064df964601
RazeSoldier <liguangjie4399@hotmail.com>
2017-11-27 18:08:45 +0800

    [PSR] 修复config/index.php行结尾有空格的问题

--- config/index.php
+++ config/index.php
@@ -1,7 +1,7 @@
 <?php
-/** 
+/**
  * 软件的安装和配置脚本文件
- * 
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
@@ -16,7 +16,7 @@
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  * http://www.gnu.org/copyleft/gpl.html
- * 
+ *
  * @file
  */
 
